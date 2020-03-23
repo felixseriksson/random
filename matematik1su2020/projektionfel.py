ccdb9579584dc390db7246fd8b46f137c04adf94
Felix Steinberger Eriksson <felix.s.eriksson@gmail.com>
2020-03-23 23:37:27 +0100

    lite nya/rättade saker

--- matematik1su2020/projektionfel.py
+++ matematik1su2020/projektionfel.py
@@ -1,3 +1,7 @@
+# viktigt meddelande till allmänheten
+# detta är helt fel och bygger på ett missförstånd i början
+# titta istället på projektion.py, istället för denna.
+# Tack det var allt
 import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -29,33 +33,30 @@ ax.plot(xspread, yzeros, zzeros, color="k")
 ax.plot(xzeros, yspread, zzeros, color="k")
 ax.plot(xzeros, yzeros, zspread, color="k")
 
-a= -10
-b = 10
-c = 5
 
 x1 = [0]
 y1 = [0]
 z1 = [0]
-xdir1 = [-10]
-ydir1 = [0]
-zdir1 = [10]
+xdir1 = [0]
+ydir1 = [14]
+zdir1 = [0]
 
 x2 = [0, 0]
 y2 = [0, 0]
 z2 = [0, 0]
-xdir2 = [a, (a-c)/2]
-ydir2 = [b, 0]
-zdir2 = [c, (c-a)/2]
+xdir2 = [-4, 0]
+ydir2 = [10, 10]
+zdir2 = [6, 0]
 
-x3 = [a]
-y3 = [b]
-z3 = [c]
-xdir3 = [(a-c)/2-a]
-ydir3 = [0-b]
-zdir3 = [(c-a)/2-c]
+x3 = [-4]
+y3 = [10]
+z3 = [6]
+xdir3 = [4]
+ydir3 = [0]
+zdir3 = [-6]
 
 #För att visa planet x-y-z=0, vänligen avkommentera rad nedan med "#"
-#'''
+'''
 #---
 
 point  = np.array([0, 0, 0])
@@ -79,11 +80,11 @@ ax.plot_surface(xx, yy, z, cmap="gray", alpha=0.5)
 #'''
 
 
-ax.quiver(x1, y1, z1, xdir1, ydir1, zdir1, length=1, arrow_length_ratio = 0.3, normalize=False)
-ax.quiver(x2, y2, z2, xdir2, ydir2, zdir2, length=1, arrow_length_ratio = 0.3, normalize=False, color="red")
-ax.quiver(x3, y3, z3, xdir3, ydir3, zdir3, length=1, arrow_length_ratio = 0.3, normalize=False, color="black", hatch="ooo", facecolor="none")
+ax.quiver(x1, y1, z1, xdir1, ydir1, zdir1, length=1, arrow_length_ratio = 0.2, normalize=False)
+ax.quiver(x2, y2, z2, xdir2, ydir2, zdir2, length=1, arrow_length_ratio = 0.2, normalize=False, color="red")
+ax.quiver(x3, y3, z3, xdir3, ydir3, zdir3, length=1, arrow_length_ratio = 0.2, normalize=False, color="black", hatch="ooo", facecolor="none")
 #för att visa den speglade vektorn, vänligen avkommentera rad nedan med "#"
-#'''
-ax.quiver(0, 0, 0, (((-a)/6) + (c/6)), (((2*a)/3) - ((2*c)/3)), ((a/6) - (c/6)), length=1, arrow_length_ratio = 0.3, normalize=False, color="green")
+'''
+ax.quiver(0, 0, 0, 6.67, 3.33, -6.67, length=1, arrow_length_ratio = 0.2, normalize=False, color="green")
 #'''
 plt.show()
